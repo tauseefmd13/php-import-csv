c2f6667bc4afea0a444061173e0997ec5f64796d
tauseefmd13 <tauseefmd13@gmail.com>
2024-11-25 16:07:21 +0530

    first commit

--- distribute.php
+++ distribute.php
@@ -1,3 +1,8 @@
 <?php  
 
+require 'db_connect.php';
+
+$totalRecords = $pdo->query("SELECT COUNT(id) AS total FROM temporary_complete_data")->fetch(PDO::FETCH_ASSOC)['total'];
+echo 'Total: ' . $totalRecords;
+
 ?>
